c9e446d43f8c9c1b1133cfbb3983d7a0632ffca1
GSC <gsc.code@gmail.com>
2024-12-12 22:13:09 +0100

    Saving progress.

---
+++ PixPorter/Classes/DirectoryReader.cs
@@ -0,0 +1,42 @@
+﻿using static Constants;
+
+public static class DirectoryReader
+{
+	public static IEnumerable<string> GetEntries(Func<string, bool> filter, string errorType)
+	{
+		var dir = Directory.GetCurrentDirectory();
+
+		try
+		{
+			var entries = errorType == "Directory"
+				? Directory.EnumerateDirectories(dir)
+				: Directory.EnumerateFiles(dir);
+
+			return entries
+				.Where(entry => (File.GetAttributes(entry) & FileAttributes.Hidden) == 0 && filter(entry))
+				.Select(Path.GetFileName)
+				.Where(name => name != null)
+				.ToList()!;
+		}
+		catch (UnauthorizedAccessException)
+		{
+			return [$"Error: Access to the path '{dir}' is denied."];
+		}
+		catch (Exception ex)
+		{
+			return [$"Error: {ex.Message}"];
+		}
+	}
+
+	public static IEnumerable<string> GetDirectories()
+	{
+		return GetEntries(_ => true, "Directory");
+	}
+
+	public static IEnumerable<string> GetImageFiles()
+	{
+		return GetEntries(file => FileFormats.SupportedFormats.Contains(Path.GetExtension(file),
+			StringComparer.OrdinalIgnoreCase),
+			"File");
+	}
+}
--- PixPorter/Classes/PixPorter.cs
+++ PixPorter/Classes/PixPorter.cs
@@ -1,7 +1,6 @@
 ﻿public class PixPorter
 {
 	private readonly CommandProcessor _commandProcessor;
-	private readonly DirectoriesService _directoriesService;
 
 	public PixPorter()
 	{
@@ -9,7 +8,6 @@
 		var commandParser = new CommandParser();
 		var commandExecutor = new CommandExecutor(converter);
 
-		_directoriesService = new DirectoriesService();
 		_commandProcessor = new CommandProcessor(commandParser, commandExecutor);
 	}
 
@@ -19,7 +17,7 @@
 
 		while (true)
 		{
-			UI.RenderUI(_directoriesService.GetDirectories(Directory.GetCurrentDirectory()), _directoriesService.GetImageFiles(Directory.GetCurrentDirectory()));
+			UI.RenderUI(DirectoryReader.GetDirectories(), DirectoryReader.GetImageFiles());
 			UI.WriteLine($"Current Directory: {Directory.GetCurrentDirectory()}");
 			var input = UI.ReadLine("Enter command:");
 
